4aa05072f79336169544b50c816a953c4edb5e05
Jakub Podsiadły <kubapodsiadly@gmail.com>
2022-10-09 16:11:01 +0200

    Styling added

--- flutter_expenses_appliacaiton/lib/widgets/transaction_list.dart
+++ flutter_expenses_appliacaiton/lib/widgets/transaction_list.dart
@@ -34,46 +34,25 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: (ctx, index) {
                 return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.black,
-                            width: 2,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '${_userTransactions[index].amount.toStringAsFixed(2)}zł',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Theme.of(context).primaryColorDark,
-                          ),
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  elevation: 5,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text('${_userTransactions[index].amount}zł'),
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            _userTransactions[index].title,
-                            style: Theme.of(context).textTheme.headline6,
-                          ),
-                          Text(
-                              DateFormat.yMMMd()
-                                  .format(_userTransactions[index].date),
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 12,
-                              )),
-                        ],
-                      ),
-                    ],
+                    ),
+                    title: Text(
+                      _userTransactions[index].title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(_userTransactions[index].date),
+                    ),
                   ),
                 );
               },
