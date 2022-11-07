50c820b439a305c33238f7764e185cbd12ffffc5
Jakub Podsiadły <kubapodsiadly@gmail.com>
2022-11-07 23:09:36 +0100

    Edit product functionality added

--- flutter_shop_application/lib/main.dart
+++ flutter_shop_application/lib/main.dart
@@ -8,6 +8,8 @@ import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './screens/cart_screen.dart';
 import './screens/orders_screen.dart';
+import './screens/user_products_screen.dart';
+import './screens/edit_product.dart';
 
 void main() => runApp(MyApp());
 
@@ -38,6 +40,8 @@ class MyApp extends StatelessWidget {
           ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
           OrdersScreen.routeName: (ctx) => OrdersScreen(),
+          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+          EditProductScreen.routeName: (ctx) => EditProductScreen(),
         },
       ),
     );
--- flutter_shop_application/lib/widgets/product_item.dart
+++ flutter_shop_application/lib/widgets/product_item.dart
@@ -49,8 +49,22 @@ class ProductItem extends StatelessWidget {
           ),
           trailing: IconButton(
             icon: Icon(Icons.shopping_cart),
-            onPressed: () =>
-                cart.addItem(product.id, product.price, product.title),
+            onPressed: () {
+              cart.addItem(product.id, product.price, product.title);
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text(
+                  'Added item to cart!',
+                ),
+                duration: Duration(seconds: 3),
+                action: SnackBarAction(
+                  label: 'UNDO',
+                  onPressed: () {
+                    cart.removeSingleItem(product.id);
+                  },
+                ),
+              ));
+            },
             color: Theme.of(context).accentColor,
           ),
         ),
